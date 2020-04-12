a494ba0bc159a07af0411fdd3d0f608ac7e1c170
jopakka <jopakkamies@gmail.com>
2020-04-12 16:53:44 +0300

    MeleeEnemy starting

---
+++ Assets/Scripts/Enemy/MeleeEnemyAttack.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeEnemyAttack : MonoBehaviour{
+    [SerializeField]
+    float timeBetweenAttacks = 0.5f;
+    [SerializeField]
+    int attackDamage = 10;
+
+    GameObject player;
+    //PlayerHealth playerHealth;
+    //EnemyHealth enemyHealth;
+    //Animator anim;
+    bool playerInRange;
+    float timer;
+
+    private void Awake() {
+        player = GameObject.FindGameObjectWithTag("Player");
+        //playerHealth = player.GetComponent<PlayerHealth>();
+        //enemyHealth = GetComponent<EnemyHealth>();
+        //anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject == player) {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.gameObject == player) {
+            playerInRange = false;
+        }
+    }
+
+    private void Update() {
+        timer += Time.deltaTime;
+
+        if(timer >= timeBetweenAttacks && playerInRange/* && enemyHealth.currentHealth > 0*/) {
+            Attack();
+        }
+
+        /*if(playerHealth.currentHealth <= 0) {
+            anim.SetTrigger("PlayerDead");
+        }*/
+    }
+
+    private void Attack() {
+        timer = 0f;
+
+        /*if(playerHealth > 0) {
+            playerHealth.TakeDamage(attackDamage);
+        }*/
+
+        Debug.Log(this.name + " harms the player");
+    }
+}
---
+++ Assets/Scripts/Enemy/MeleeEnemyMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MeleeEnemyMovement : MonoBehaviour {
+    GameObject player;
+    NavMeshAgent nav;
+    //EnemyHealth enemyHealth;
+    //PlayerHealth playerHealth;
+    bool playerInRange;
+
+    private void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+        nav = GetComponent<NavMeshAgent>();
+        //enemyhealth = GetComponent<EnemyHealth>();
+        //playerHealth = GetComponent<playerHealth>();
+    }
+
+    private void Update() {
+        //if(enemyHealth > 0 && playerHealth > 0) {
+        if (playerInRange) {
+            nav.isStopped = true;
+        } else {
+            nav.isStopped = false;
+            nav.SetDestination(player.transform.position);
+        } //else
+        //  nav.enabled = false;
+        //}
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject == player) {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject == player) {
+            playerInRange = false;
+        }
+    }
+}
