666b5d78f3798abb5ba0fe64cc7ea265fc84e3af
jopakka <jopakkamies@gmail.com>
2020-05-04 13:05:38 +0300

    player can die

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using System.Runtime.Remoting.Messaging;
+
+public class PlayerHealth : MonoBehaviour {
+    public int startingHealth = 100;
+    public int currentHealth;
+    //public Slider healthSlider;
+
+    Animator anim;
+    CharacterMovement playerMovement;
+    Weapon playerShooting;
+    bool isDead;
+    bool damaged;
+
+    void Start() {
+        anim = GetComponent<Animator>();
+        playerMovement = GetComponent<CharacterMovement>();
+        playerShooting = GetComponentInChildren<Weapon>();
+        currentHealth = startingHealth;
+    }
+    void Update() {
+
+        damaged = false;
+    }
+    public void TakeDamage(int amount) {
+        damaged = true;
+        currentHealth -= amount;
+        //healthSlider.value = currentHealth;
+
+        if (currentHealth <= 0 && !isDead) {
+            Death();
+        }
+    }
+
+    void Death() {
+        isDead = true;
+        //anim.SetTrigger("Die");
+
+        //playerMovement.enabled = false;
+        //playerShooting.enabled = false;
+    }
+
+    public bool IsDead {
+        get => isDead;
+    }
+}
