eaf94f7bf519001bf83f8d6f774766c79106d53e
eerohem <63100886+eerohem@users.noreply.github.com>
2020-05-05 12:52:19 +0300

    Added triggers for melee enemies

---
+++ Assets/SpawnTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTrigger : MonoBehaviour
+{
+    [SerializeField]
+    GameObject spawnPoint;
+    GameObject Player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            spawnPoint.SetActive(true);
+        }
+    }
+}
